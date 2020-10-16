9cfcd2fac803b995239ab98039658b4aa34e0fbd
joaogab <joaogabrieldecarvalho.com@gmail.com>
2020-10-16 14:57:41 -0300

    aula 02 | ajuste tabela orphanages no banco de dados

--- backend/src/database/migrations/1602868486566-create_orphanages.ts
+++ backend/src/database/migrations/1602868486566-create_orphanages.ts
@@ -38,6 +38,10 @@ export class createOrphanages1602868486566 implements MigrationInterface {
             type: 'text',
           },
           {
+            name: 'opening_hours',
+            type: 'varchar',
+          },
+          {
             name: 'open_on_weekends',
             type: 'boolean',
             default: false,
