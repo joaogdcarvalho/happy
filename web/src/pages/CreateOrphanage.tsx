4886fe6452a57314613d1c810b1e55928ca29597
joaogab <joaogabrieldecarvalho.com@gmail.com>
2020-10-17 15:52:28 -0300

    aula 03 | criação de um orfanato

--- web/src/pages/CreateOrphanage.tsx
+++ web/src/pages/CreateOrphanage.tsx
@@ -7,8 +7,12 @@ import Sidebar from "../components/Sidebar";
 import mapIcon from "../utils/mapIcon";
 
 import '../styles/pages/create-orphanage.css';
+import api from "../services/api";
+import { useHistory } from "react-router-dom";
 
 export default function OrphanagesMap() {
+  const history = useHistory();
+
   const [position, setPosition] = useState({ latitude: 0, longitude: 0});
 
   const [name, setName] = useState('');
@@ -44,8 +48,30 @@ export default function OrphanagesMap() {
     setPreviewImages(selectedImagesPreview);
   }
 
-  function handleSubmit(event: FormEvent) {
+  async function handleSubmit(event: FormEvent) {
     event.preventDefault();
+
+    const { latitude, longitude } = position;
+
+    const data = new FormData();
+
+    data.append('name', name);
+    data.append('about', about);
+    data.append('latitude', String(latitude));
+    data.append('longitude', String(longitude));
+    data.append('instructions', instructions);
+    data.append('opening_hours', opening_hours);
+    data.append('open_on_weekends', String(open_on_weekends));
+
+    images.forEach(image => {
+      data.append('images', image);
+    })
+
+    await api.post('orphanages', data);
+
+    alert('Cadastro realizado com sucesso!');
+
+    history.push('/app');
   }
 
   return (
